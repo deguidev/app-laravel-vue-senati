7e4fca4b2b121bf3b42bc185744700fa10b9fd37
deguidev <dlp.diegolipa@gmail.com>
2025-12-02 11:12:39 -0500

    metodo put

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -3,51 +3,77 @@
 namespace App\Http\Controllers;
 
 use App\Models\Categoria;
+use Exception;
 use Illuminate\Http\Request;
 
 class CategoriaController extends Controller
 {
     //CRUD
     //LISTAR
-    public function listarCategoria(){
+    public function listarCategoria()
+    {
         //$categorias = Categoria::all();
-        $categorias = Categoria::orderBy('created_at','desc')->get();
+        $categorias = Categoria::orderBy('created_at', 'desc')->get();
 
         return response()->json([
-                'success'=> true,
-                'nombre' => 'Diego Lipa',
-                'data' => $categorias
-            ]);
+            'success' => true,
+            'nombre' => 'Diego Lipa',
+            'data' => $categorias
+        ]);
     }
 
-    public function guardarCategoria(Request $request){
+    public function guardarCategoria(Request $request)
+    {
         $validate = $request->validate([
             'descripcion' => 'required|string',
-            'nombre_categoria'=>'required'
+            'nombre_categoria' => 'required'
         ]);
 
-        $categoria = Categoria::create($request->all());
+        try {
+            $categoria = Categoria::create($request->all());
 
-        return response()->json([
-                'success'=> true,
+            return response()->json([
+                'success' => true,
                 'nombre' => 'Diego Lipa',
                 'data' => $categoria
             ]);
+        } catch (Exception $e) {
+            return response()->json([
+                'success' => false,
+                'nombre' => 'Diego Lipa',
+                'data' => $e->getMessage(),
+            ]);
+        }
     }
 
-    public function editarCategoria(){
+    public function editarCategoria(Request $request, $id_categoria) {
+        $validate = $request->validate([
+            'descripcion' => 'required|string',
+            'nombre_categoria' => 'required'
+        ]);
 
-    }
+        $categoria = Categoria::find($id_categoria);
 
-    public function eliminarCategoria($id_categoria){
-        $categoria = Categoria::findOrFail($id_categoria);
-        $categoria->delete();
-
+        $categoria->update([
+            'descripcion' => $request->descripcion,
+            'nombre_categoria' => $request->nombre_categoria,
+        ]);
         return response()->json([
-            'success'=> true,
+            'success' => true,
             'nombre' => 'Diego Lipa',
             'data' => $categoria
         ]);
     }
 
+    public function eliminarCategoria($id_categoria)
+    {
+        $categoria = Categoria::findOrFail($id_categoria);
+        $categoria->delete();
+
+        return response()->json([
+            'success' => true,
+            'nombre' => 'Diego Lipa',
+            'data' => $categoria
+        ]);
+    }
 }
--- resources/js/pages/Categoria.vue
+++ resources/js/pages/Categoria.vue
@@ -11,6 +11,7 @@ import { onMounted, ref } from 'vue';
 const categorias = ref([]);
 const miNombre = ref('');
 const mostrarModal = ref(false);
+const mostrarModalEditar = ref(false);
 
 //Formulario
 const formulario = ref({
@@ -44,12 +45,30 @@ const cerrarModal = () => {
     mostrarModal.value = false;
 };
 
+//funciones para manipular el modal editar
+const abrirModalEditar = ( dataCategoria : any ) => {
+    mostrarModalEditar.value = true;
+
+    console.log(dataCategoria.nombre_categoria);
+    console.log(dataCategoria.descripcion);
+    console.log(dataCategoria);
+
+    formulario.value.nombre_categoria = dataCategoria.nombre_categoria;
+    formulario.value.descripcion = dataCategoria.descripcion;
+
+};
+
+const cerrarModalEditar = () => {
+    mostrarModalEditar.value = false;
+};
+
+
 const enviarFormulario = async () => {
     console.log('Diego Lipa');
     console.log(formulario.value);
 
     const respuesta = await axios.post('/categorias-data', formulario.value);
-    if(respuesta.data.success){
+    if (respuesta.data.success) {
         Swal.fire({
             title: 'Recurso Creado',
             text: 'Categoria Creada',
@@ -57,7 +76,30 @@ const enviarFormulario = async () => {
         });
         mostrarModal.value = false;
         listarCategoria();
-    }else{
+    } else {
+        Swal.fire({
+            title: 'Error al crear',
+            text: 'Categoria no creada',
+            icon: 'error',
+        });
+    }
+};
+
+
+const actualizarFormulario = async () => {
+    console.log('Diego Lipa');
+    console.log(formulario.value);
+
+    const respuesta = await axios.put('/categorias-data', formulario.value);
+    if (respuesta.data.success) {
+        Swal.fire({
+            title: 'Recurso Creado',
+            text: 'Categoria Creada',
+            icon: 'success',
+        });
+        mostrarModal.value = false;
+        listarCategoria();
+    } else {
         Swal.fire({
             title: 'Error al crear',
             text: 'Categoria no creada',
@@ -81,18 +123,18 @@ const eliminarCategoria = async (id: number) => {
 
 const confirmacion = (id: number) => {
     Swal.fire({
-    title: "Estas Seguro?",
-    text: "El recurso se eliminará de forma permanente",
-    icon: "warning",
-    showCancelButton: true,
-    confirmButtonColor: "#3085d6",
-    cancelButtonColor: "#d33",
-    confirmButtonText: "Apectar",
-    cancelButtonText: "Cancelar"
+        title: 'Estas Seguro?',
+        text: 'El recurso se eliminará de forma permanente',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Apectar',
+        cancelButtonText: 'Cancelar',
     }).then((result) => {
-    if (result.isConfirmed) {
-        eliminarCategoria(id);
-    }
+        if (result.isConfirmed) {
+            eliminarCategoria(id);
+        }
     });
 };
 
@@ -109,86 +151,6 @@ onMounted(listarCategoria);
                 <small>{{ miNombre }}</small>
             </div>
             <div>
-                <div>
-                    <a
-                        class="group relative inline-flex items-center overflow-hidden rounded-sm border border-current px-8 py-3 text-indigo-600 dark:text-white"
-                        href="#"
-                        @click="abrirModal"
-                    >
-                        <span
-                            class="absolute -start-full transition-all group-hover:start-4"
-                        >
-                            <Plus />
-                        </span>
-                        <span
-                            class="text-sm font-medium transition-all group-hover:ms-4"
-                        >
-                            Agregar
-                        </span>
-                    </a>
-                </div>
-                <div class="overflow-x-auto">
-                    <table
-                        class="min-w-full divide-y-2 divide-gray-200 dark:divide-gray-700"
-                    >
-                        <thead class="ltr:text-left rtl:text-right">
-                            <tr
-                                class="*:font-medium *:text-gray-900 dark:*:text-white"
-                            >
-                                <th class="px-3 py-2 whitespace-nowrap">
-                                    Nombre
-                                </th>
-                                <th class="px-3 py-2 whitespace-nowrap">
-                                    Descripción
-                                </th>
-                                <th class="px-3 py-2 whitespace-nowrap">
-                                    Estado
-                                </th>
-                                <th class="px-3 py-2 whitespace-nowrap">
-                                    Opciones
-                                </th>
-                            </tr>
-                        </thead>
-
-                        <tbody
-                            class="divide-y divide-gray-200 *:even:bg-gray-50 dark:divide-gray-700 dark:*:even:bg-gray-800"
-                        >
-                            <tr
-                                v-for="item in categorias"
-                                :key="item.id"
-                                class="*:text-gray-900 *:first:font-medium dark:*:text-white"
-                            >
-                                <td class="px-3 py-2 whitespace-nowrap">
-                                    {{ item.nombre_categoria }}
-                                </td>
-                                <td class="px-3 py-2 whitespace-nowrap">
-                                    {{ item.descripcion }}
-                                </td>
-                                <td class="px-3 py-2 whitespace-nowrap">
-                                    {{ item.estado }}
-                                </td>
-                                <td class="px-3 py-2 whitespace-nowrap">
-                                    <div class="flex flex-row gap-4">
-                                        <a
-                                            class="inline-block rounded-sm border border-current px-8 py-3 text-sm font-medium text-indigo-600 transition hover:scale-110 hover:rotate-2"
-                                            href="#"
-                                        >
-                                            <Pencil />
-                                        </a>
-                                        <a
-                                            class="inline-block rounded-sm border border-current px-8 py-3 text-sm font-medium text-rose-600 transition hover:scale-110 hover:rotate-2"
-                                            href="#"
-                                            @click="confirmacion(item.id)"
-                                        >
-                                            <Trash2 />
-                                        </a>
-                                    </div>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-
                 <!-- Modal registro categoria -->
                 <div
                     class="fixed inset-0 z-50 grid place-content-center bg-black/50 p-4"
@@ -257,6 +219,157 @@ onMounted(listarCategoria);
                         </form>
                     </div>
                 </div>
+
+                <!-- Modal Editar Categoria -->
+                <div
+                    class="fixed inset-0 z-50 grid place-content-center bg-black/50 p-4"
+                    role="dialog"
+                    aria-modal="true"
+                    aria-labelledby="modalTitle"
+                    v-if="mostrarModalEditar"
+                >
+                    <div
+                        class="w-full max-w-md rounded-lg bg-white p-6 shadow-lg dark:bg-gray-900"
+                    >
+                        <h2
+                            id="modalTitle"
+                            class="text-xl font-bold text-gray-900 sm:text-2xl dark:text-white"
+                        >
+                            Editar Categoria
+                        </h2>
+
+                        <form class="mt-4" @submit.prevent="enviarFormulario">
+                            <div class="mb-3">
+                                <label for="nombre_categoria">
+                                    <span
+                                        class="text-sm font-medium text-gray-700 dark:text-gray-200"
+                                    >
+                                        Nombre
+                                    </span>
+                                    <input
+                                        type="text"
+                                        id="nombre_categoria"
+                                        v-model="formulario.nombre_categoria"
+                                        class="mt-0.5 w-full rounded border-gray-300 p-2 shadow-sm sm:text-sm dark:border-gray-600 dark:bg-gray-900 dark:text-white"
+                                    />
+                                </label>
+                            </div>
+                            <div class="mb-3">
+                                <label for="descripcion">
+                                    <span
+                                        class="text-sm font-medium text-gray-700 dark:text-gray-200"
+                                    >
+                                        Descripcion
+                                    </span>
+                                    <input
+                                        type="text"
+                                        id="descripcion"
+                                        v-model="formulario.descripcion"
+                                        class="mt-0.5 w-full rounded border-gray-300 p-2 shadow-sm sm:text-sm dark:border-gray-600 dark:bg-gray-900 dark:text-white"
+                                    />
+                                </label>
+                            </div>
+                            <footer class="mt-6 flex justify-end gap-2">
+                                <button
+                                    type="button"
+                                    class="flex items-center justify-center gap-2 rounded bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700 transition-colors hover:bg-gray-200 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-gray-700"
+                                    @click="cerrarModalEditar"
+                                >
+                                    <SquareX /> Cancel
+                                </button>
+
+                                <button
+                                    type="submit"
+                                    class="flex items-center justify-center gap-2 rounded bg-blue-600 px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-blue-700"
+                                >
+                                    <Save /> Guardar
+                                </button>
+                            </footer>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="mx-2 md:mx-10 lg:mx-20">
+            <div class="mb-3">
+                <a
+                    class="group relative inline-flex items-center overflow-hidden rounded-sm border border-current px-8 py-3 text-indigo-600 dark:text-white"
+                    href="#"
+                    @click="abrirModal"
+                >
+                    <span
+                        class="absolute -start-full transition-all group-hover:start-4"
+                    >
+                        <Plus />
+                    </span>
+                    <span
+                        class="text-sm font-medium transition-all group-hover:ms-4"
+                    >
+                        Agregar
+                    </span>
+                </a>
+            </div>
+
+            <div
+                class="bg-neutral-primary-soft rounded-base border-default relative overflow-x-auto border shadow-xs"
+            >
+                <table
+                    class="text-body w-full text-left text-sm rtl:text-right"
+                >
+                    <thead
+                        class="text-body bg-neutral-secondary-soft rounded-base border-default border-b text-sm"
+                    >
+                        <tr>
+                            <th scope="col" class="px-6 py-3 font-medium">
+                                Nombre
+                            </th>
+                            <th scope="col" class="px-6 py-3 font-medium">
+                                Descripción
+                            </th>
+                            <th scope="col" class="px-6 py-3 font-medium">
+                                Estado
+                            </th>
+                            <th scope="col" class="px-6 py-3 font-medium">
+                                Opciones
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr
+                            v-for="item in categorias"
+                            :key="item.id"
+                            class="bg-neutral-primary border-default border-b"
+                        >
+                            <th
+                                scope="row"
+                                class="text-heading px-6 py-4 font-medium whitespace-nowrap"
+                            >
+                                {{ item.nombre_categoria }}
+                            </th>
+                            <td class="px-6 py-4">{{ item.descripcion }}</td>
+                            <td class="px-6 py-4">{{ item.estado }}</td>
+                            <td class="px-6 py-4">
+                                <div class="flex flex-row gap-4">
+                                    <a
+                                        class="inline-block rounded-sm border border-current px-8 py-3 text-sm text-indigo-600 transition hover:scale-110 hover:rotate-2"
+                                        href="#"
+                                        @click="abrirModalEditar(item)"
+                                    >
+                                        <Pencil />
+                                    </a>
+                                    <a
+                                        class="inline-block rounded-sm border border-current px-8 py-3 text-sm font-medium text-rose-600 transition hover:scale-110 hover:rotate-2"
+                                        href="#"
+                                        @click="confirmacion(item.id)"
+                                    >
+                                        <Trash2 />
+                                    </a>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </AppLayout>
